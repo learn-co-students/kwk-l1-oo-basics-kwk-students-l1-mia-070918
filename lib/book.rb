5d6c64d8fa2547f5452fbf417bed653193fa85d9
jadahinds <jhinds1229@pinescharter.net>
2018-07-12 17:51:45 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,31 @@
 # book.rb
 # Add you Book class here
+class Book
+  def initialize(title)
+    @title=title
+  end
+  def title
+    return @title
+  end
+  def author=(author)
+    @author=author
+  end
+  def author
+    return @author
+  end
+  def page_count=(page_count)
+    @page_count=page_count
+  end
+  def page_count
+    @page_count
+  end
+  def genre=(genre)
+    @genre=genre
+  end
+  def genre
+    @genre
+  end
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
